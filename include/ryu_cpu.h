fc906fac4d59514a80a7fb1b383c63403c076040
Soulaymen Chouri <praisethemoon@users.noreply.github.com>
2017-03-30 19:03:19 +0200

    Update ryu_cpu.h

--- include/ryu_cpu.h
+++ include/ryu_cpu.h
@@ -6,6 +6,18 @@
 #ifndef RYU_CPU_H
 #define RYU_CPU_H
 
+#include <stddef.h>
 
+#include <ryu_types.h>
+#include <ryu_stack.h>
+#include <ryu_program.h>
+
+typedef struct RyuCPU {
+	size_t programCounter;
+	RyuProgram* program;
+	RyuStack* stack;
+};
+
+void cpu_run(RyuCPU* cpu, RyuProgram* program);
 
 #endif
