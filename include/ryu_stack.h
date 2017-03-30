e2ca5261093bef13dc496af89430bec42fc3d895
Soulaymen Chouri <praisethemoon@users.noreply.github.com>
2017-03-30 15:41:54 +0200

    Create ryu_stack.h

---
+++ include/ryu_stack.h
@@ -0,0 +1,30 @@
+/*
+ * RYU STACK
+ */
+
+#ifndef RYU_STACK_H
+#define RYU_STACK_H
+
+#include <ryu_types.h>
+
+#define INIT_STACK_SIZE 1024*1024
+#define MAX_STACK_SIZE 1024*1024*50
+
+typedef struct RyuStack {
+size_t currentSize;
+	void* mem;
+};
+
+void pushB(Byte data);
+void pushW(Word data);
+void pushDW(DWord data);
+void pushQW(QWord data);
+void pushDQW(DQWord data);
+
+Byte   popB();
+Word   popW();
+DWord  popDW();
+QWord  popQW();
+DQWord popDQW();
+
+#endif
