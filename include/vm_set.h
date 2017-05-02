eb0ab520cf29d79bdc14a3776abe9018d5543513
praisethemoon <doit@praisethemoon.org>
2017-05-02 13:43:31 +0100

    ++

--- include/vm_set.h
+++ include/vm_set.h
@@ -30,4 +30,54 @@
 
 #define REG_SIZE 16
 
+#define PUSH_B   0
+#define PUSH_W   1
+#define PUSH_DW  2
+#define PUSH_QW  3
+#define PUSH_DQW 4
 
+#define POP_B   10
+#define POP_W   11
+#define POP_DW  12
+#define POP_QW  13
+#define POP_DQW 14
+
+#define MOV_REG_B   20
+#define MOV_REG_W   21
+#define MOV_REG_DW  22
+#define MOV_REG_QW  23
+#define MOV_REG_DQW 24
+
+#define MOV_MEM_B   30
+#define MOV_MEM_W   31
+#define MOV_MEM_DW  32
+#define MOV_MEM_QW  33
+#define MOV_MEM_DQW 34
+
+#define MOV_MEM_REG_B   40
+#define MOV_MEM_REG_W   41
+#define MOV_MEM_REG_DW  42
+#define MOV_MEM_REG_QW  43
+#define MOV_MEM_REG_DQW 44
+
+#define MOV_REG_MEM_B   50
+#define MOV_REG_MEM_W   51
+#define MOV_REG_MEM_DW  52
+#define MOV_REG_MEM_QW  53
+#define MOV_REG_MEM_DQW 54
+
+#define ADD_U_B   60
+#define ADD_U_W   61
+#define ADD_U_DW  62
+#define ADD_U_QW  63
+#define ADD_U_DQW 64
+
+#define ADD_S_B   70
+#define ADD_S_W   71
+#define ADD_S_DW  72
+#define ADD_S_QW  73
+#define ADD_S_DQW 74
+
+
+
+#endif
---
+++ source/vm_set.c
@@ -0,0 +1,6 @@
+#include "ryu.h"
+#include "vm_set.h"
+
+unsigned int buildInstruction(unsigned char instruction, unsigned char arg1, unsigned char arg2) {
+	
+}
