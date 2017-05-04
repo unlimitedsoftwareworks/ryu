2cee583d3babeda495b4f38d58a95b71728bb534
praisethemoon <doit@praisethemoon.org>
2017-05-05 01:50:00 +0200

    preparing another test

--- source/main.c
+++ source/main.c
@@ -13,41 +13,20 @@
 int main(int argc, char* argv[]) {
 	printf(RYU_CMD_PROMPT, RYU_VERSION);
 	
-	RyuCPU* cpu = RyuCPU_new();
+	RyuCPU* cpu0 = RyuCPU_new();
 	
-	RyuProgram program;
-	program.data = NULL;
-	program.lineCount = 0;
-	program.code = dmt_calloc(1, sizeof(uint64_t**));
+	RyuProgram program0;
+	program0.data = NULL;
+	program0.lineCount = 0;
+	program0.code = dmt_calloc(1, sizeof(uint64_t**));
 	
-	addInstruction(&program, PUSH_B, 255, 0);
-	addInstruction(&program, POP_B, R15, 0);
-	addInstruction(&program, PRINT_REG, R15, 0);
-	addInstruction(&program, PRINT_CHAR, '\n', 0);
-	addInstruction(&program, HALT, 0, 0);
+	addInstruction(&program0, PUSH_B, 255, 0);
+	addInstruction(&program0, POP_B, R15, 0);
+	addInstruction(&program0, PRINT_REG, R15, 0);
+	addInstruction(&program0, PRINT_CHAR, '\n', 0);
+	addInstruction(&program0, HALT, 0, 0);
 	
-	
-	/*
-	program.code = dmt_calloc(4, sizeof(uint64_t*));
-	
-	program.code[0] = dmt_calloc(3, sizeof(uint64_t));
-	program.code[0][0] = PUSH_B;
-	program.code[0][1] = 10;
-	
-	program.code[1] = dmt_calloc(3, sizeof(uint64_t));
-	program.code[1][0] = POP_B;
-	program.code[1][1] = R15;
-
-	program.code[2] = dmt_calloc(3, sizeof(uint64_t));
-	program.code[2][0] = PRINT_REG;
-	program.code[2][1] = R15;
-	
-	program.code[3] = dmt_calloc(3, sizeof(uint64_t));
-	program.code[3][0] = HALT;
-	*/
-	
-	
-	RyuCPU_run(cpu, &program);
+	RyuCPU_run(cpu0, &program0);
 	
 	return 0;
 }
